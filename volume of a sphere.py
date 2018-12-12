cd0bd6622354548584a9f5aa4c832dc0d4585d77
Souravdg <45494334+Souravdg@users.noreply.github.com>
2018-12-12 12:39:37 -0500

    Add files via upload

---
+++ volume of a sphere.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+#Calculate the volume of a sphere
+import math
+d=10
+r=d/2
+PI=math.exp(math.pi)
+V=(4/3)*PI*r**3
+print("Volume of the sphere: %.4f" %V)
+
